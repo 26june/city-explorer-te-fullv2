2a2b4896cbe34ab3b5238efd1f74c8a7b9adc831
26june <gj.abordo1@gmail.com>
2023-11-02 13:57:28 +0000

    add axios

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
@@ -7,5 +8,16 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Welcome to my API!"));
+
+app.get("/weather", async (req, res) => {
+  let { lat = 35.7796, lon = -78.6382 } = req.query;
+  console.log(process.env.WEATHERBIT_MASTER_KEY);
+
+  const axiosRes =
+    await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHERBIT_MASTER_KEY}
+    `);
+
+  res.send(axiosRes.data);
+});
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
