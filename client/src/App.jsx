50243ec3aaed5fe43d1b5ae3a16a47a32f0c8b9d
26june <gj.abordo1@gmail.com>
2023-11-02 13:48:04 +0000

    create search component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import MyMap from "./components/MyMap";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
+      <Search></Search>
       <MyMap></MyMap>
     </div>
   );
