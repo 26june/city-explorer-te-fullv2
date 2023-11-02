10e63fc1695b7e0a0125d95196a6f1d5e6bfef29
26june <gj.abordo1@gmail.com>
2023-11-02 14:02:24 +0000

    add location endpoint

--- server/server.js
+++ server/server.js
@@ -11,13 +11,23 @@ app.get("/", (req, res) => res.send("Welcome to my API!"));
 
 app.get("/weather", async (req, res) => {
   let { lat = 35.7796, lon = -78.6382 } = req.query;
-  console.log(process.env.WEATHERBIT_MASTER_KEY);
 
   const axiosRes =
     await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHERBIT_MASTER_KEY}
     `);
 
   res.send(axiosRes.data);
+});
+
+app.get("/location", async (req, res) => {
+  //   let { lat = 35.7796, lon = -78.6382 } = req.query;
+  let search = "London";
+
+  const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${process.env.LOC_API_KEY}&format=json`;
+
+  const axiosRes = await axios.get(API);
+
+  res.send(axiosRes.data[0]);
 });
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
