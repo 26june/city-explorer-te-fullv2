cf05e7d9f72db9b528c1ec103021723c3a3cd6e9
26june <gj.abordo1@gmail.com>
2023-11-02 15:38:47 +0000

    add weather info

--- client/src/components/WeatherInfo.jsx
+++ client/src/components/WeatherInfo.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
-export default function WeatherInfo(currentSearch) {
+export default function WeatherInfo({ currentSearch }) {
   const { lat, lon } = currentSearch;
-  
-  return <div className="WeatherInfo">WeatherInfo</div>;
+
+  const [currentData, setCurrentData] = useState({});
+
+  useEffect(() => {
+    async function getData() {
+      const axiosRes = await axios.get(
+        `http://localhost:8080/weather?lat=${lat}&lon=${lon}`
+      );
+
+      setCurrentData(axiosRes.data.data[0]);
+      console.log(axiosRes.data.data[0]);
+    }
+
+    getData();
+  }, [currentSearch]);
+
+  return (
+    <div className="WeatherInfo">
+      WeatherInfo
+      <div className="testText">
+        <p>{currentData.city_name}</p>
+        <p>{currentData.temp}</p>
+        <p>{currentData.weather?.description}</p>
+      </div>
+    </div>
+  );
 }
