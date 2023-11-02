09acbba841287c3f253f3df7e9c5bd184e237d99
26june <gj.abordo1@gmail.com>
2023-11-02 13:42:45 +0000

    init server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Welcome to my API!"));
+
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
